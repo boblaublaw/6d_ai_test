db536d6882f3c9516944b1e94526b2ff829bce5e
boblaublaw <github@sublimation.org>
2018-10-11 13:05:36 -0700

    added game controller and updated object

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public Camera ARCamera;
+
+public GameObject ballReference;
+
+private List<GameObject> balls;
+
+void Start()
+   {
+       balls = new List<GameObject>();
+   }
+
+void Update()
+   {
+       if (Input.touchCount > 0)
+       {
+           Touch touch = Input.GetTouch(0);
+           LaunchBall(touch);
+       }
+   }
+
+void LaunchBall(Touch touch)
+   {
+       if (EventSystem.current.currentSelectedGameObject == null &&
+           touch.phase == TouchPhase.Began)
+       {
+           Vector3 position = new Vector3(Input.mousePosition.x,  Input.mousePosition.y, .5f);
+           position = ARCamera.ScreenToWorldPoint(position);
+           GameObject ball = Instantiate(ballReference, position, ballReference.transform.rotation);
+           balls.Add(ball);
+       }
+   }
 }
